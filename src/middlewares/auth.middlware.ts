600203884c3a4d8c292f7b225405179d8b297095
mederdev <matkalykov.m@icloud.com>
2022-10-13 14:13:14 +0600

    updated middleware

--- src/middlewares/auth.middlware.ts
+++ src/middlewares/auth.middlware.ts
@@ -1,24 +1,27 @@
-import { Injectable, NestMiddleware, Req } from "@nestjs/common";
+import { Injectable, NestMiddleware, Req, Res } from "@nestjs/common";
 import { NextFunction, Response } from "express";
 import { TokenService } from 'src/token/token.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
 	constructor(private tokenService: TokenService) { }
-	use(@Req() req: any, res: Response, next: NextFunction) {
+	use(@Req() req: any, @Res() res: any, next: NextFunction) {
 		try {
 			const authorizationHeader = req.headers.authorization;
 			if (!authorizationHeader) {
+				res.send("Пустой токен")
 				return next('Empty token');
 			}
 
 			const accessToken = authorizationHeader.split(' ')[1];
 			if (!accessToken) {
+				res.send("Неккоректный токен")
 				return next('Incorrectly token');
 			}
 
 			const userData = this.tokenService.validateAccessToken(accessToken);
 			if (!userData) {
+				res.send("Еще не авторизован")
 				return next('Unauthentificated');
 			}
 
