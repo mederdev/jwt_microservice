123e36491d152d3d55556060ad9949498b533e0f
mederdev <matkalykov.m@icloud.com>
2022-10-13 13:43:32 +0600

    .env updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,9 @@ import { UserModule } from './user/user.module';
 import { RepostModule } from './repost/repost.module';
 import { ConfigModule } from '@nestjs/config';
 import { TokenModule } from './token/token.module';
-
+require('dotenv').config();
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://mederbek:barcelona@cluster0.0scib9d.mongodb.net/?retryWrites=true&w=majority'), ConfigModule.forRoot(), UserModule, TokenModule, RepostModule],
+  imports: [MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.0scib9d.mongodb.net/?retryWrites=true&w=majority`), ConfigModule.forRoot(), UserModule, TokenModule, RepostModule],
   controllers: [AppController],
   providers: [AppService],
 })
